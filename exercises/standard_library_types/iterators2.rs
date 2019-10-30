3b6d6df336f426fe51dc2ce7338c7ad947bb3733
Adam Melnyk <adam@adammelnyk.ca>
2019-10-29 20:06:37 -0500

    iterators

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -9,8 +9,17 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.collect::<String>() + c.as_str(),
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
+}
+
+pub fn capitalize_words(input: Vec<String>) -> Vec<String> {
+    let mut v = Vec::new();
+    for i in input {
+        let s = capitalize_first(&i);
+        v.push(s.to_string());
+    }
+    v
 }
 
 #[cfg(test)]
@@ -29,115 +38,31 @@ mod tests {
         assert_eq!(capitalize_first(""), "");
     }
 
-    // Step 2.
+    //Step 2.
     #[test]
     fn test_iterate_string_vec() {
-        let words = vec!["hello", "world"];
-        let capitalized_words: Vec<String> = // TODO
+        let words = vec!["hello".to_string(), "world".to_string()];
+        let capitalized_words: Vec<String> = capitalize_words(words);
         assert_eq!(capitalized_words, ["Hello", "World"]);
     }
 
     #[test]
     fn test_iterate_into_string() {
-        let words = vec!["hello", " ", "world"];
-        let capitalized_words = // TODO
+        let words = vec!["hello".to_string(), " ".to_string(), "world".to_string()];
+        let capitalized_words: String = capitalize_words(words).into_iter().collect();
         assert_eq!(capitalized_words, "Hello World");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Step 1
 // You need to call something on `first` before it can be collected
 // Currently its type is `char`. Have a look at the methods that are available on that type:
 // https://doc.rust-lang.org/std/primitive.char.html
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Step 2
 // First you'll need to turn the Vec into an iterator
 // Then you'll need to apply your function unto each item in the vector
 // P.s. Don't forget to collect() at the end!
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Step 3.
 // This is very similar to the previous test. The only real change is that you will need to
